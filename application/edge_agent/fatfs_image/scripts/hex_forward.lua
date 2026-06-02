0237213552e5cdf37e51f1795825a72cc767f124
ebbeelfvin-rgb <ebbe.elfvin@gmail.com>
2026-06-02 07:04:22 +0200

    .

--- application/edge_agent/fatfs_image/scripts/hex_forward.lua
+++ application/edge_agent/fatfs_image/scripts/hex_forward.lua
@@ -3,8 +3,8 @@ local delay = require("delay")
 local u = uart.new(1, 47, 48, 115200)
 if u:write("B&") then
     delay.delay_ms(500) then
-    u:write("C|0|0|0&")
+    u:write("C|0|50|0&")
 
     elif u:write("C|0|50|0&")
-    
+
 return "Går frammåt!"
